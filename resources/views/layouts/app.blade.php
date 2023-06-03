aba9d678379e386baa30443c54a381f6ab38f56a
Nevil Pitroda <nevilpitroda@gmail.com>
2023-06-03 12:17:16 +0530

    change menu style

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -119,60 +119,60 @@
                     </div>
                     <div class="offcanvas-body">
                         <ul class="nav d-block p-0 m-0">
-                            <li class="list-item">
+                            <li class="list-item m-2">
                                 <a href="#" class="nav-link">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#6362e7" class="bi bi-house" viewBox="0 0 16 16">
                                         <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5ZM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5 5 5Z"/>
                                     </svg>
-                                    <span class="p-3 fs-6 fw-bold text-black">Dashboard</span>
+                                    <span class="p-3 fs-5 p-1 fw-bold text-black">Dashboard</span>
                                 </a>
-                                <ul class="subnav text-dark">
-                                    <li><a href="{{ url('home') }}" class="text-decoration-none p-2 text-dark">Home</a></li>
-                                    <li><a href="{{ url('product') }}" class="text-decoration-none p-2 text-dark">Product</a></li>  
-                                    <li><a href="{{ url('category') }}" class="text-decoration-none p-2 text-dark">Category</a></li>  
+                                <ul class="subnav text-dark ms-3">
+                                    <li class="m-1"><a href="{{ url('home') }}" class="text-decoration-none p-2 text-dark">Home</a></li>
+                                    <li class="m-1"><a href="{{ url('product') }}" class="text-decoration-none p-2 text-dark">Product</a></li>  
+                                    <li class="m-1"><a href="{{ url('category') }}" class="text-decoration-none p-2 text-dark">Category</a></li>  
                                 </ul>
                             </li>
-                            <li class="list-item">
+                            <li class="list-item m-2">
                                 <a href="#" class="nav-link">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#6362e7" class="bi bi-box" viewBox="0 0 16 16">
                                         <path d="M8.186 1.113a.5.5 0 0 0-.372 0L1.846 3.5 8 5.961 14.154 3.5 8.186 1.113zM15 4.239l-6.5 2.6v7.922l6.5-2.6V4.24zM7.5 14.762V6.838L1 4.239v7.923l6.5 2.6zM7.443.184a1.5 1.5 0 0 1 1.114 0l7.129 2.852A.5.5 0 0 1 16 3.5v8.662a1 1 0 0 1-.629.928l-7.185 2.874a.5.5 0 0 1-.372 0L.63 13.09a1 1 0 0 1-.63-.928V3.5a.5.5 0 0 1 .314-.464L7.443.184z"/>
                                     </svg>
-                                    <span class="p-3 fs-6 fw-bold text-black">Widgets</span>
+                                    <span class="p-3 fs-5 p-1 fw-bold text-black">Widgets</span>
                                 </a>
-                                <ul class="subnav text-dark">
-                                    <li><a href="#" class="text-decoration-none p-2 text-dark">General</a></li>
-                                    <li><a href="#" class="text-decoration-none p-2 text-dark">Chart</a></li>
+                                <ul class="subnav text-dark ms-3">
+                                    <li class="m-1"><a href="#" class="text-decoration-none p-2 text-dark">General</a></li>
+                                    <li class="m-1"><a href="#" class="text-decoration-none p-2 text-dark">Chart</a></li>
                                 </ul>
                             </li>
-                            <li class="list-item">
+                            <li class="list-item m-2">
                                 <a href="#" class="nav-link">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#6362e7" class="bi bi-card-checklist" viewBox="0 0 16 16">
                                         <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"/>
                                         <path d="M7 5.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0zM7 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 0 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0z"/>
                                     </svg>
-                                    <span class="p-3 fs-6 fw-bold text-black">Page Layout</span>
+                                    <span class="p-3 fs-5 p-1 fw-bold text-black">Page Layout</span>
                                 </a>
-                                <ul class="subnav text-dark">
-                                    <li><a href="#" class="text-decoration-none p-2 text-dark">Boxed</a></li>
-                                    <li><a href="#" class="text-decoration-none p-2 text-dark">RTL</a></li>
-                                    <li><a href="#" class="text-decoration-none p-2 text-dark">Dark</a></li>
+                                <ul class="subnav text-dark ms-3">
+                                    <li  class="m-1"><a href="#" class="text-decoration-none p-2 text-dark">Boxed</a></li>
+                                    <li  class="m-1"><a href="#" class="text-decoration-none p-2 text-dark">RTL</a></li>
+                                    <li  class="m-1"><a href="#" class="text-decoration-none p-2 text-dark">Dark</a></li>
                                 </ul>
                             </li>
-                            <li class="list-item">
+                            <li class="list-item m-2">
                                 <a href="#" class="nav-link">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#6362e7" class="bi bi-person-fill" viewBox="0 0 16 16">
                                         <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
                                     </svg>
-                                    <span class="p-3 fs-6 fw-bold text-black">User</span>
+                                    <span class="p-3 fs-5 p-1 fw-bold text-black">User</span>
                                 </a>
-                                <ul class="subnav  list-unstyled text-dark">
+                                <ul class="subnav  list-unstyled text-dark ms-3">
                                     <li class="list-item">
                                         <a href="{{ route('register') }}" class="nav-link">
                                             <svg xmlns="http://www.w3.org/2000/svg"  width="20" height="20" fill="#6362e7" class="bi bi-box-arrow-in-right" viewBox="0 0 16 16">
                                                 <path fill-rule="evenodd" d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0v-2z"/>
                                                 <path fill-rule="evenodd" d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
                                             </svg>
-                                            <span class="p-3 fs-6 text-black">{{ __('Register') }}</span>
+                                            <span class="p-3 fs-5 p-1 text-black">{{ __('Register') }}</span>
                                         </a>
                                     </li>
 
@@ -182,7 +182,7 @@
                                                 <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"/>
                                                 <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
                                             </svg>
-                                            <span class="p-3 fs-6 text-black">{{ __('Login') }}</span>
+                                            <span class="p-3 fs-5 p-1 text-black">{{ __('Login') }}</span>
                                         </a>
                                     </li>
                             
@@ -194,7 +194,7 @@
                                                     <path fill-rule="evenodd" d="M10 3.5a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-2a.5.5 0 0 1 1 0v2A1.5 1.5 0 0 1 9.5 14h-8A1.5 1.5 0 0 1 0 12.5v-9A1.5 1.5 0 0 1 1.5 2h8A1.5 1.5 0 0 1 11 3.5v2a.5.5 0 0 1-1 0v-2z"/>
                                                     <path fill-rule="evenodd" d="M4.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H14.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3z"/>
                                                 </svg>
-                                                <span class="p-3 fs-6 text-black">{{ __('Logout') }}</span>
+                                                <span class="p-3 fs-5 p-1 text-black">{{ __('Logout') }}</span>
                                         </a>
                                         <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                             @csrf
